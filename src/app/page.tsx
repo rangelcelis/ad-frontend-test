da018f1fa1520d8b378149abcd26ef431e68e6a7
Alejandro <rangel.celis@gmail.com>
2025-01-02 11:14:39 -0300

    test: Vitest initial setup and first basic test.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default async function Home() {
+const Home = () => {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24 font-bold text-4xl text-blue-600'>
-      Hello, world!
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-bold text-4xl text-blue-600">
+      <h1>Hello, world!</h1>
     </main>
-  )
-}
+  );
+};
+
+export default Home;
