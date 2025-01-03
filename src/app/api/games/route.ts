3263c4832afedbf7beca7f9655e2419ac44b8579
Alejandro <rangel.celis@gmail.com>
2025-01-03 13:14:01 -0300

    fix(api): "totalPages" always returned the value related to "allGames" without validation of "genre" filter.

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -1,18 +1,16 @@
-import { allGames, availableFilters, delay } from "@/utils/endpoint";
+import { allGames, availableFilters, delay } from '@/utils/endpoint';
 
 const ITEMS_PER_PAGE = 12;
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
-  const genre = searchParams.get("genre");
-  let page = parseInt(searchParams.get("page") ?? "1");
+  const genre = searchParams.get('genre');
+  let page = parseInt(searchParams.get('page') ?? '1');
 
   let games = allGames;
 
   if (genre) {
-    games = games.filter(
-      (game) => game.genre.toLowerCase() === genre.toLowerCase()
-    );
+    games = games.filter((game) => game.genre.toLowerCase() === genre.toLowerCase());
   }
 
   if (page < 1 || isNaN(page)) page = 1;
@@ -22,10 +20,10 @@ export async function GET(request: Request) {
 
   const fromIndex = (page - 1) * ITEMS_PER_PAGE;
   const toIndex = page * ITEMS_PER_PAGE;
-  games = games.slice(fromIndex, toIndex);
+  const currentGames = games.slice(fromIndex, toIndex);
 
-  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(games.length / ITEMS_PER_PAGE);
   const currentPage = page;
 
-  return Response.json({ games, availableFilters, totalPages, currentPage });
+  return Response.json({ availableFilters, totalPages, currentPage, games: currentGames });
 }
