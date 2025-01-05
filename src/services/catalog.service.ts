164e5a1072a5cd9e3bf13a115affcef11215793d
Alejandro <rangel.celis@gmail.com>
2025-01-05 13:55:51 -0300

    fix(services): API URL in client side readed from .env file

--- src/services/catalog.service.ts
+++ src/services/catalog.service.ts
@@ -1,5 +1,5 @@
 export const getGames = async (page: number, genre: string) => {
-  const url = new URL(process.env.API_BASE_URL || 'http://localhost:3000//api/games');
+  const url = new URL(process.env.NEXT_PUBLIC_API_BASE_URL || '');
   url.searchParams.append('page', page.toString());
 
   if (genre && genre !== 'all') {
@@ -18,7 +18,7 @@ export const getGames = async (page: number, genre: string) => {
 };
 
 export const getGenres = async () => {
-  const response = await fetch(process.env.API_BASE_URL || 'http://localhost:3000//api/games');
+  const response = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL || '');
   const { availableFilters } = await response.json();
 
   return availableFilters;
